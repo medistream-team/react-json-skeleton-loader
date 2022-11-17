8bba103a50d7ea2377f42e7c90113c1a13ee3bfb
JeongHyeonSeok <hyeonseok45@gmail.com>
2022-11-17 13:34:29 +0900

    스타일링을 제외한 틀 형식 중 box, title 완료

--- src/SkeletonLoader.tsx
+++ src/SkeletonLoader.tsx
@@ -1,56 +1,69 @@
-import {useState} from "react";
+import { useState, useEffect, useCallback } from "react";
+import styled from "styled-components";
 
-const SkeletonLoader: React.FC = () => {
-const [skeletonLoader, setSkeletonLoader] = useState<string[][]>([])
-setSkeletonLoader([['box', 'title:5']])
+const SkeletonLoader: React.FC = (props) => {
+  const [skeletonLoader, setSkeletonLoader] = useState<string[][]>([]);
 
-// "text : 5 " 와 같은 형식일 경우 해당 콜론 뒤의 숫자 만큼 for 문이 돌아가는 걸 상정 하여
-// 해당 함수에서는 뒤의 숫자를 바깥으로 빼내는것이 주요 관건임
-// const textColonNumber = (element) => {
-//     if(element.includes(':')){
-//         const elementName = element.split(':')[0];
-//         const count = element.split(':')[1];
-//         const elementChildren = [];
-//         for(let i = 0; i < count; i++){
-//         }
-//     }
-// }
+  const buttonHandler = useCallback(() => {
+    setSkeletonLoader([["box", "text:5", "box"]])
+  },[skeletonLoader])
+  console.log(skeletonLoader);
 
-    return (
-        <div>
-            {/* skeletonLoader 안의 값으로 배열들이 있고 해당 배열 은 map 메소드를 이용해 item 으로 취급된다. 각 item 의 값 안에 'box','title', 'text' 등 이 존재할 경우에 각각의 스타일을 가진 요소들이 생성이 되는 로직 // item = ['box', 'title:5']*/}
-            {skeletonLoader.map( item  => 
-                item.map(element => {
-                // item의 요소들 중에 "box" 가 존재할 경우
-                if(element.includes("box")){
-                    <div style={} />
+  return (
+    <div>
+      <button onClick={buttonHandler}>버튼</button>
+      {/* skeletonLoader 안의 값으로 배열들이 있고 해당 배열 은 map 메소드를 이용해 item 으로 취급된다. 각 item 의 값 안에 'box','title', 'text' 등 이 존재할 경우에 각각의 스타일을 가진 요소들이 생성이 되는 로직 // item = ['box', 'title:5']*/}
+      {skeletonLoader?.map((item: string[], index) => (
+        <div key={index}>
+          {item?.map((element: string, index) => {
+            console.log(element);
+            // item의 요소들 중에 "box" 가 존재할 경우
+            if (element.includes("box")) {
+              return <S.Box key={index}/>;
+            }
+            // item의 요소들 중에 "title" 이 존재할 경우
+            if (element.includes("title")) {
+              return <S.Title key={index}/>;
+            }
+            // tiem의 요소들 중에 "text" 가 존재할 경우 && "text : 5 " 와 같은 형식일 경우 해당 콜론 뒤의 숫자 만큼 for 문이 돌아가는 걸 상정 해야함
+            if (element.includes("text")) {
+              if (element.includes(":")) {
+                // const elementName: string = element.split(":")[0];
+                const count: number = Number(element.split(":")[1]);
+                // const elementChildren: string[] = [];
+                // for (let i = 0; i < count; i++) {
+                //   elementChildren.push(elementName);
+                // }
+                for (let i = 0; i < count; i++) {
+                  console.log(i);
+                  <S.Text key={index}/>;
                 }
-                // item의 요소들 중에 "title" 이 존재할 경우
-                if(element.includes("title")){
-                    <div style={} />
-                }
-                // tiem의 요소들 중에 "text" 가 존재할 경우 && "text : 5 " 와 같은 형식일 경우 해당 콜론 뒤의 숫자 만큼 for 문이 돌아가는 걸 상정 해야함
-                if(element.includes("text")){
-                    if(element.includes(':')){
-                        const elementName: string = element.split(':')[0];
-                        const count: number = Number(element.split(':')[1]);
-                        const elementChildren: string[] = [];
-                        for(let i = 0; i < count; i++){
-                            elementChildren.push(elementName)
-                        }
-                    for(let i = 0; i < count; i++){
-                        <div></div>
-                    }
-                }
-
-                        }
-                        return <div />
-                }  )
-            )
-        }
+              }
+            }
+            return <div key={index}/>;
+          })}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
+const S = {
+  Box: styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: black;
+  `,
+  Title: styled.div`
+    width: 80px;
+    height: 15px;
+    background-color: black;
+  `,
+  Text: styled.div`
+    width: 200px;
+    height: 12px;
+    background-color: black;
+  `,
+};
 
 export default SkeletonLoader;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import SkeletonLoader from './SkeletonLoader'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <SkeletonLoader />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
