f312b0ad9529ec33d331834f8f95964d7d9e6de6
JeongHyeonSeok <hyeonseok45@gmail.com>
2022-12-02 14:51:18 +0900

    npm 배포작업 최종 (배포 전)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Testing from './Testing';
+import JsonSkeletonLoader from './JsonSkeletonLoader';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(rootElement);
 
-root.render(<Testing />);
+root.render(<JsonSkeletonLoader />);
