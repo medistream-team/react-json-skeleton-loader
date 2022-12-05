8dff509127a96ffde5a6a7ff0da97e1cf853763f
JeongHyeonSeok <hyeonseok45@gmail.com>
2022-12-05 23:40:34 +0900

    Example & 에디터를 제외한 css작업 완료

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,24 +1,194 @@
-import logo from './logo.svg';
-import './App.css';
+import JsonSkeletonLoader from 'react-json-skeleton-loader'
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [defaultSkeleton, setDefaultSkeleton] = useState(
+    [['box', 'title + text:4'], ['text:3']]
+  )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className='header'>
+        <img
+          className='skeleton-loader-img'
+          src='/img/SkeletonLoader.svg'
+          alt='skeletonLoaderLogo'
+        />
+        <h1 className='skeleton-loader-text'>React Json Skeleton Loader</h1>
+        <div className='skeleton-loader-link'>A loader component that can be designed with simple markup. - <a href='https://github.com/medistream-team/react-skeleton-loader'><strong>GitHub</strong></a></div>
       </header>
-    </div>
+      <main>
+        <section className='section-default'>
+          <div className='main-section-default'>
+            <div className='main-section-default-skeletonloader'>
+              <JsonSkeletonLoader
+                defaultSizes={{
+                  box: 100,
+                  circle: 100,
+                }}
+                content={defaultSkeleton}
+              />
+            </div>
+            <textarea
+              className='main-section-default-textarea'
+            />
+          </div>
+        </section>
+        <section className='section-getting-started'>
+          <h2>Getting Started</h2>
+          <h3>Installation</h3>
+          <div className='getting-started-install'>
+            <div className='getting-started-install-code'>
+              npm install <span className='npm-name'>react-json-skeleton-loader</span>
+            </div>
+          </div>
+          <div className='getting-started-import'>
+            <div className='getting-started-import-code'>
+              import <span className='npm-name'>JsonSkeletonLoader</span> from <span className='npm-name'>'react-json-skeleton-loader'</span>
+            </div>
+          </div>
+        </section>
+        <section className='section-examples'>
+          <h2>Examples</h2>
+          <h3>Text with thumbnail</h3>
+          <div>
+            <input type='text' />
+            <JsonSkeletonLoader
+              defaultSizes={{
+                box: 100,
+                circle: 100,
+              }}
+              content={[['box', 'title + text:3']]} />
+          </div>
+
+          <h3>Text with avatar</h3>
+          <div>
+            <input type='text' />
+            <JsonSkeletonLoader
+              defaultSizes={{
+                box: 100,
+                circle: 100,
+              }}
+              content={[['circle', 'title + text:3']]} />
+          </div>
+
+          <h3>Text with figure</h3>
+          <div>
+            <input type='text' />
+            <JsonSkeletonLoader
+              defaultSizes={{
+                box: 100,
+                circle: 100,
+              }}
+              content={[['box', 'title + text:3']]} />
+          </div>
+          <div>
+            <input type='text' />
+            <JsonSkeletonLoader
+              defaultSizes={{
+                box: 100,
+                circle: 100,
+              }}
+              content={[['box', 'title + text:3']]} />
+          </div>
+        </section>
+        <section className='section-properties'>
+          <div className='section-properties-container'>
+            <h2>Properties (options)</h2>
+            <div className='options-name'><b>Disable animation</b></div>
+            <div className='options-container'>
+              <div className='options-container-item'>
+                <JsonSkeletonLoader
+                  defaultSizes={{
+                    box: 70,
+                    circle: 100,
+                  }}
+                  options={{
+                    animation: false
+                  }}
+                  content={[['box', 'text:3']]}
+                />
+              </div>
+              <div className='options-code'>
+                <code>
+                  {`<JsonSkeletonLoader options = {{animation: false}} />`}
+                </code>
+              </div>
+            </div>
+
+            <div className='options-name'><b>Animation speed</b></div>
+            <div className='options-container'>
+              <div className='options-container-item'>
+                <JsonSkeletonLoader
+                  defaultSizes={{
+                    box: 70,
+                    circle: 100,
+                  }}
+                  options={{
+                    speed: 0.5
+                  }}
+                  content={[['box', 'text:3']]}
+                />
+              </div>
+              <div className='options-code'>
+                <code>
+                  {`<JsonSkeletonLoader options = {{speed: 0.5}} />`}
+                </code>
+              </div>
+            </div>
+
+            <div className='options-name'><b>Radius</b></div>
+            <div className='options-container'>
+              <div className='options-container-item'>
+                <JsonSkeletonLoader
+                  defaultSizes={{
+                    box: 70,
+                    circle: 100,
+                  }}
+                  options={{
+                    radius: 15
+                  }}
+                  content={[['box', 'text:3']]}
+                />
+              </div>
+              <div className='options-code'>
+                <code>
+                  {`<JsonSkeletonLoader options = {{radius: 15}} />`}
+                </code>
+              </div>
+            </div>
+
+            <div className='options-name'><b>Colors</b></div>
+            <div className='options-container'>
+              <div className='options-container-item'>
+                <JsonSkeletonLoader
+                  defaultSizes={{
+                    box: 70,
+                    circle: 100,
+                  }}
+                  options={{
+                    primaryColor: '#ffb0b0',
+                    secondaryColor: '#4c8bf5'
+                  }}
+                  content={[['box', 'text:3']]}
+                />
+              </div>
+              <div className='options-code'>
+                <code>
+                  {`<JsonSkeletonLoader options = {{primaryColor: '#ffb0b0', secondaryColor: '#4c8bf5'}} />`}
+                </code>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className='footer-text'>Copyright © 2022. Built by INTEGRATION Corp.</div>
+        <div className='footer-text'>This project is distributed under MIT license.</div>
+      </footer>
+    </>
   );
 }
 
